bfe13efc4995c3f41c0b4c2e6f278308f19fd4e5
vinoth58543 <145655177+vinoth58543@users.noreply.github.com>
2023-09-25 14:52:18 +0530

    Update pat task 5(1).py

--- pat task 5(1).py
+++ pat task 5(1).py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+# Input string
+input_string = "Guvi Geeks Network Private Limited"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Convert the input string to lowercase to handle both uppercase and lowercase vowels
+input_string = input_string.lower()
 
+# Initialize counts for each vowel
+count_a = 0
+count_e = 0
+count_i = 0
+count_o = 0
+count_u = 0
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Iterate through the string to count each vowel
+for char in input_string:
+    if char == 'a':
+        count_a += 1
+    elif char == 'e':
+        count_e += 1
+    elif char == 'i':
+        count_i += 1
+    elif char == 'o':
+        count_o += 1
+    elif char == 'u':
+        count_u += 1
 
+# Calculate the total number of vowels
+total_vowels = count_a + count_e + count_i + count_o + count_u
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Print the results
+print("Total vowels:", total_vowels)
+print("Count of 'A':", count_a)
+print("Count of 'E':", count_e)
+print("Count of 'I':", count_i)
+print("Count of 'O':", count_o)
+print("Count of 'U':", count_
